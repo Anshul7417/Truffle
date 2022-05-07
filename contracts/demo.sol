2a306d142c8060052620223dfca38db0eb9b098e
Anshul Tripathi <tripathianshul06@gmail.com>
2022-05-07 23:56:15 +0530

    Deployment on Ganache

--- contracts/demo.sol
+++ contracts/demo.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract demo{
     uint number;
     function set(uint _number) public{
-        number=_number;
+        number=_number+1;
     }
 
     function get() public view returns(uint){
